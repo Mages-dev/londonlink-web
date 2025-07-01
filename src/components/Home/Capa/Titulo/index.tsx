70a2f7a6536ea6c028b5f345125d5164ab620ad4
Rafael Vasconcelos <6738341+Youkko@users.noreply.github.com>
2025-07-01 01:45:15 -0300

    Fixed Capa text styling

--- src/components/Home/Capa/Titulo/index.tsx
+++ src/components/Home/Capa/Titulo/index.tsx
@@ -1,9 +1,10 @@
-import styles from "../../Home.module.css"
+import parentStyles from "../../Home.module.css"
+import styles from "../Capa.module.css"
 
 const Titulo: React.FC = () => {
 	return (
     <div className={`${styles.textBlock}`}>
-      <h1 className={`${styles.h1} ${styles.shadow}`}>Você quer<br />aprender <span className={`${styles.bold}`}>inglês?</span></h1>
+      <h1 className={`${styles.h1} ${styles.shadow}`}>Você quer<br />aprender <span className={`${parentStyles.bold}`}>inglês?</span></h1>
       <p className={`${styles.paragrafo}`}>Diga-nos o que você precisa<br />e criaremos um curso só para você!</p>
     </div>
 	)
